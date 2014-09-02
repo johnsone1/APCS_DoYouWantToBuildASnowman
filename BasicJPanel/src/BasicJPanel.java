c3600bb50cf0614d1cff58d986a1bfbe91ee2211
Eric Johnosn <eric.johnson12321@gmail.com>
2014-09-02 11:35:09 -0400

    A Snowthingymabobber

--- BasicJPanel/src/BasicJPanel.java
+++ BasicJPanel/src/BasicJPanel.java
@@ -10,7 +10,7 @@
 
 // Import the basic graphics classes.
 import java.awt.*;
-import javax.swing.*;
+        import javax.swing.*;
 
 public class BasicJPanel extends JPanel{
 
@@ -28,8 +28,8 @@ public class BasicJPanel extends JPanel{
       made visible or moved or whatever.
     */
     public void paintComponent(Graphics g){
-        g.drawLine(10,10,450,450); // Draw a line from (10,10) to (150,150)
-        g.drawOval(200, 200, 50, 50);
+        g.drawOval(175, 150, 150, 150);
+        g.drawOval(215, 70, 70, 84);
     }
 
     public static void main(String arg[]){
